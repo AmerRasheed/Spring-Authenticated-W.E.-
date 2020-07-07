4f918360f164a6812c9a793e9e0be24a9fe60844
AmerRasheed <59695079+AmerRasheed@users.noreply.github.com>
2020-07-07 11:43:48 +0200

    Initial Comment.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ Spring IOC Project (W.E.)
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Spring Project IOC" );
+    }
+}
