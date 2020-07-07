46393db71b4aaa9f040343622680c27a56f424e2
AmerRasheed <59695079+AmerRasheed@users.noreply.github.com>
2020-07-07 11:59:51 +0200

    XML_IOC Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.example.data.AppUserDAO;
+import org.example.model.AppUser;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  Spring IOC Project (W.E.)
  */
@@ -8,5 +12,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Spring Project IOC" );
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        AppUserDAO appUserDAO= context.getBean(AppUserDAO.class);
+
+        System.out.println(appUserDAO.save(new AppUser(null,"testemail","testname","testaddress")));
+
+        context.close();
     }
 }
